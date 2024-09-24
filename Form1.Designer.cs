db0f434fae4dd2f1a26136700e8d728491c7534c
Parsa Basiri <79114292+parsabasiri@users.noreply.github.com>
2024-09-25 00:25:23 +0330

    Tic Tac Toe Game With C# (Windows Form) - Practical DataBase Programming

---
+++ Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace My_Tic_Tac_toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            Label1 = new Label();
+            lblTurn = new Label();
+            Label2 = new Label();
+            label3 = new Label();
+            lblPlayer1Score = new Label();
+            lblPlayer2Score = new Label();
+            btnPlayAgain = new Button();
+            btnRestGame = new Button();
+            Label4 = new Label();
+            Label5 = new Label();
+            pnlMain = new Panel();
+            pnlMain.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.FlatAppearance.BorderColor = SystemColors.Highlight;
+            button1.FlatAppearance.BorderSize = 2;
+            button1.FlatAppearance.MouseOverBackColor = SystemColors.ControlLight;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Times New Roman", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(6, 5);
+            button1.Name = "button1";
+            button1.Size = new Size(60, 60);
+            button1.TabIndex = 1;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.FlatAppearance.BorderColor = SystemColors.Highlight;
+            button2.FlatAppearance.BorderSize = 2;
+            button2.FlatAppearance.MouseOverBackColor = SystemColors.ControlLight;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Times New Roman", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(65, 5);
+            button2.Name = "button2";
+            button2.Size = new Size(60, 60);
+            button2.TabIndex = 2;
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.FlatAppearance.BorderColor = SystemColors.Highlight;
+            button3.FlatAppearance.BorderSize = 2;
+            button3.FlatAppearance.MouseOverBackColor = SystemColors.ControlLight;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Times New Roman", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.Location = new Point(124, 5);
+            button3.Name = "button3";
+            button3.Size = new Size(60, 60);
+            button3.TabIndex = 3;
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.FlatAppearance.BorderColor = SystemColors.Highlight;
+            button4.FlatAppearance.BorderSize = 2;
+            button4.FlatAppearance.MouseOverBackColor = SystemColors.ControlLight;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Times New Roman", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button4.Location = new Point(6, 64);
+            button4.Name = "button4";
+            button4.Size = new Size(60, 60);
+            button4.TabIndex = 4;
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.FlatAppearance.BorderColor = SystemColors.Highlight;
+            button5.FlatAppearance.BorderSize = 2;
+            button5.FlatAppearance.MouseOverBackColor = SystemColors.ControlLight;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Times New Roman", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button5.Location = new Point(65, 64);
+            button5.Name = "button5";
+            button5.Size = new Size(60, 60);
+            button5.TabIndex = 5;
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.FlatAppearance.BorderColor = SystemColors.Highlight;
+            button6.FlatAppearance.BorderSize = 2;
+            button6.FlatAppearance.MouseOverBackColor = SystemColors.ControlLight;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Font = new Font("Times New Roman", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button6.Location = new Point(124, 64);
+            button6.Name = "button6";
+            button6.Size = new Size(60, 60);
+            button6.TabIndex = 6;
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.FlatAppearance.BorderColor = SystemColors.Highlight;
+            button7.FlatAppearance.BorderSize = 2;
+            button7.FlatAppearance.MouseOverBackColor = SystemColors.ControlLight;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Font = new Font("Times New Roman", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button7.Location = new Point(6, 123);
+            button7.Name = "button7";
+            button7.Size = new Size(60, 60);
+            button7.TabIndex = 7;
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.FlatAppearance.BorderColor = SystemColors.Highlight;
+            button8.FlatAppearance.BorderSize = 2;
+            button8.FlatAppearance.MouseOverBackColor = SystemColors.ControlLight;
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.Font = new Font("Times New Roman", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button8.Location = new Point(65, 123);
+            button8.Name = "button8";
+            button8.Size = new Size(60, 60);
+            button8.TabIndex = 8;
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.FlatAppearance.BorderColor = SystemColors.Highlight;
+            button9.FlatAppearance.BorderSize = 2;
+            button9.FlatAppearance.MouseOverBackColor = SystemColors.ControlLight;
+            button9.FlatStyle = FlatStyle.Flat;
+            button9.Font = new Font("Times New Roman", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button9.Location = new Point(124, 123);
+            button9.Name = "button9";
+            button9.Size = new Size(60, 60);
+            button9.TabIndex = 9;
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // Label1
+            // 
+            Label1.BackColor = SystemColors.ControlDark;
+            Label1.BorderStyle = BorderStyle.FixedSingle;
+            Label1.Font = new Font("B Titr", 12F, FontStyle.Bold, GraphicsUnit.Point, 178);
+            Label1.Location = new Point(199, 5);
+            Label1.Name = "Label1";
+            Label1.Size = new Size(140, 31);
+            Label1.TabIndex = 10;
+            Label1.Text = "نوبت";
+            Label1.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // lblTurn
+            // 
+            lblTurn.BackColor = Color.Transparent;
+            lblTurn.BorderStyle = BorderStyle.FixedSingle;
+            lblTurn.Font = new Font("B Titr", 10F, FontStyle.Bold, GraphicsUnit.Point, 178);
+            lblTurn.Location = new Point(199, 41);
+            lblTurn.Name = "lblTurn";
+            lblTurn.Size = new Size(140, 31);
+            lblTurn.TabIndex = 11;
+            lblTurn.Text = "بازیکن اول";
+            lblTurn.TextAlign = ContentAlignment.TopCenter;
+            lblTurn.Click += label2_Click;
+            // 
+            // Label2
+            // 
+            Label2.BackColor = Color.MediumSpringGreen;
+            Label2.Font = new Font("B Titr", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 178);
+            Label2.Location = new Point(268, 73);
+            Label2.Name = "Label2";
+            Label2.Size = new Size(70, 31);
+            Label2.TabIndex = 12;
+            Label2.Text = "بازیکن 1";
+            Label2.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            label3.BackColor = Color.Gold;
+            label3.Font = new Font("B Titr", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 178);
+            label3.Location = new Point(199, 73);
+            label3.Name = "label3";
+            label3.Size = new Size(70, 31);
+            label3.TabIndex = 13;
+            label3.Text = "بازیکن 2";
+            label3.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // lblPlayer1Score
+            // 
+            lblPlayer1Score.BackColor = Color.Transparent;
+            lblPlayer1Score.BorderStyle = BorderStyle.FixedSingle;
+            lblPlayer1Score.Font = new Font("B Titr", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 178);
+            lblPlayer1Score.Location = new Point(268, 103);
+            lblPlayer1Score.Name = "lblPlayer1Score";
+            lblPlayer1Score.Size = new Size(70, 31);
+            lblPlayer1Score.TabIndex = 14;
+            lblPlayer1Score.Text = "0";
+            lblPlayer1Score.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // lblPlayer2Score
+            // 
+            lblPlayer2Score.BackColor = Color.Transparent;
+            lblPlayer2Score.BorderStyle = BorderStyle.FixedSingle;
+            lblPlayer2Score.Font = new Font("B Titr", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 178);
+            lblPlayer2Score.Location = new Point(199, 103);
+            lblPlayer2Score.Name = "lblPlayer2Score";
+            lblPlayer2Score.Size = new Size(70, 31);
+            lblPlayer2Score.TabIndex = 15;
+            lblPlayer2Score.Text = "0";
+            lblPlayer2Score.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // btnPlayAgain
+            // 
+            btnPlayAgain.BackColor = Color.DeepSkyBlue;
+            btnPlayAgain.Font = new Font("B Titr", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 178);
+            btnPlayAgain.Location = new Point(199, 142);
+            btnPlayAgain.Name = "btnPlayAgain";
+            btnPlayAgain.Size = new Size(141, 35);
+            btnPlayAgain.TabIndex = 16;
+            btnPlayAgain.Text = "بازی مجدد";
+            btnPlayAgain.TextAlign = ContentAlignment.TopCenter;
+            btnPlayAgain.UseVisualStyleBackColor = false;
+            btnPlayAgain.Click += btnPlayAgain_Click;
+            // 
+            // btnRestGame
+            // 
+            btnRestGame.BackColor = Color.Red;
+            btnRestGame.Font = new Font("B Titr", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 178);
+            btnRestGame.Location = new Point(198, 180);
+            btnRestGame.Name = "btnRestGame";
+            btnRestGame.Size = new Size(141, 35);
+            btnRestGame.TabIndex = 17;
+            btnRestGame.Text = "ریست کردن بازی";
+            btnRestGame.TextAlign = ContentAlignment.TopCenter;
+            btnRestGame.UseVisualStyleBackColor = false;
+            btnRestGame.Click += btnRestGame_Click;
+            // 
+            // Label4
+            // 
+            Label4.BackColor = Color.MediumSpringGreen;
+            Label4.Font = new Font("B Titr", 9F, FontStyle.Bold, GraphicsUnit.Point, 178);
+            Label4.Location = new Point(84, 197);
+            Label4.Name = "Label4";
+            Label4.Size = new Size(106, 22);
+            Label4.TabIndex = 18;
+            Label4.Text = "بازیکن1: X";
+            Label4.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // Label5
+            // 
+            Label5.BackColor = Color.Gold;
+            Label5.Font = new Font("B Titr", 9F, FontStyle.Bold, GraphicsUnit.Point, 178);
+            Label5.Location = new Point(2, 197);
+            Label5.Name = "Label5";
+            Label5.Size = new Size(86, 22);
+            Label5.TabIndex = 19;
+            Label5.Text = "بازیکن2: O";
+            Label5.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // pnlMain
+            // 
+            pnlMain.Controls.Add(button9);
+            pnlMain.Controls.Add(button8);
+            pnlMain.Controls.Add(button7);
+            pnlMain.Controls.Add(button6);
+            pnlMain.Controls.Add(button5);
+            pnlMain.Controls.Add(button4);
+            pnlMain.Controls.Add(button3);
+            pnlMain.Controls.Add(button2);
+            pnlMain.Controls.Add(button1);
+            pnlMain.Location = new Point(3, 5);
+            pnlMain.Name = "pnlMain";
+            pnlMain.Size = new Size(190, 190);
+            pnlMain.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(352, 228);
+            Controls.Add(pnlMain);
+            Controls.Add(Label5);
+            Controls.Add(Label4);
+            Controls.Add(btnRestGame);
+            Controls.Add(btnPlayAgain);
+            Controls.Add(lblPlayer2Score);
+            Controls.Add(lblPlayer1Score);
+            Controls.Add(label3);
+            Controls.Add(Label2);
+            Controls.Add(lblTurn);
+            Controls.Add(Label1);
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Tic Tac Toe";
+            Load += Form1_Load;
+            pnlMain.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Label Label1;
+        private Label lblTurn;
+        private Label Label2;
+        private Label label3;
+        private Label lblPlayer1Score;
+        private Label lblPlayer2Score;
+        private Button btnPlayAgain;
+        private Button btnRestGame;
+        private Label Label4;
+        private Label Label5;
+        private Panel pnlMain;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,205 @@
+﻿namespace My_Tic_Tac_toe
+{
+    public partial class Form1 : Form
+    {
+        public int turn = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("لطفا منتطر لود شدن بازی بمانید.");
+        }
+        void Reset()
+        {
+            turn = 0;
+            foreach (Button btn in pnlMain.Controls)
+            {
+                btn.BackColor = SystemColors.Control;
+                btn.Text = string.Empty;
+                btn.Enabled = true;
+            }
+        }
+
+        void Action(string btnName)
+        {
+            foreach (Button btn in pnlMain.Controls)
+            {
+                if (btn.Name == btnName)
+                {
+                    if (turn % 2 == 0)
+                    {
+                        btn.BackColor = System.Drawing.Color.MediumSpringGreen;
+                        btn.Text = "X";
+                        lblTurn.Text = "بازیکن دوم";
+                    }
+                    else
+                    {
+                        btn.BackColor = System.Drawing.Color.Gold;
+                        btn.Text = "O";
+                        lblTurn.Text = "بازیکن اول";
+                    }
+                    btn.Enabled = false;
+                    pnlMain.Focus();
+                    break;
+                }
+            }
+        }
+        private void btnRestGame_Click(object sender, EventArgs e)
+        {
+
+            DialogResult result = MessageBox.Show("بازی ریست شود؟", "", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                Reset();
+                lblTurn.Text = "بازیکن اول";
+                lblPlayer1Score.Text = lblPlayer2Score.Text = "0";
+            }
+
+        }
+
+        private void btnPlayAgain_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("بازی شروع شود؟", "", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                lblTurn.Text = "بازیکن اول";
+                Reset();
+            }
+        }
+        bool CheckVertical(List<Button> player)
+        {
+            if ((button1.Text == "X" && button4.Text == "X" && button7.Text == "X") || (button2.Text == "X" && button5.Text == "X" && button8.Text == "X") || (button3.Text == "X" && button6.Text == "X" && button9.Text == "X"))
+                return true;
+            if ((button1.Text == "O" && button4.Text == "O" && button7.Text == "O") || (button2.Text == "O" && button5.Text == "O" && button8.Text == "O") || (button3.Text == "O" && button6.Text == "O" && button9.Text == "O"))
+                return true;
+            return false;
+        }
+        bool CheckHorizantal(List<Button> player)
+        {
+            if ((button1.Text == "X" && button2.Text == "X" && button3.Text == "X") || (button4.Text == "X" && button5.Text == "X" && button6.Text == "X") || (button7.Text == "X" && button8.Text == "X" && button9.Text == "X"))
+                return true;
+            if ((button1.Text == "O" && button2.Text == "O" && button3.Text == "O") || (button4.Text == "O" && button5.Text == "O" && button6.Text == "O") || (button7.Text == "O" && button8.Text == "O" && button9.Text == "O"))
+                return true;
+            return false;
+        }
+        bool CheckDiagonal(List<Button> player)
+        {
+            if (button5.Text == "X" && ((button1.Text == "X" && button9.Text == "X") || (button3.Text == "X" && button7.Text == "X")))
+                return true;
+            if (button5.Text == "O" && ((button1.Text == "O" && button9.Text == "O") || (button3.Text == "O" && button7.Text == "O")))
+                return true;
+            return false;
+        }
+        bool isAllButtonsDisable()
+        {
+            foreach (Button btn in pnlMain.Controls)
+            {
+                if (btn.Enabled == true) return false;
+            }
+            return true;
+        }
+        void Check()
+        {
+            List<Button> player1 = new List<Button>();
+            List<Button> player2 = new List<Button>();
+            foreach (Button btn in pnlMain.Controls)
+            {
+                if (btn.Text == "X") player1.Add(btn);
+                else if (btn.Text == "O") player2.Add(btn);
+            }
+            if (turn % 2 == 0)
+            {
+                if (CheckVertical(player1) || CheckHorizantal(player1) || CheckDiagonal(player1))
+                {
+                    MessageBox.Show("بازیکن اول برنده شد", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    lblPlayer1Score.Text = (Convert.ToInt16(lblPlayer1Score.Text) + 1).ToString();
+                    Reset();
+                    return;
+                }
+            }
+            else
+            {
+                if (CheckVertical(player2) || CheckHorizantal(player2) || CheckDiagonal(player2))
+                {
+                    MessageBox.Show("بازیکن دوم برنده شد", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    lblPlayer2Score.Text = (Convert.ToInt16(lblPlayer2Score.Text) + 1).ToString();
+                    Reset();
+                    return;
+                }
+            }
+            if (isAllButtonsDisable() == true)
+            {
+                MessageBox.Show("این دست از بازی برنده نداشت!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Reset();
+            }
+            turn++;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Action("button1");
+            Check();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Action("button2");
+            Check();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Action("button3");
+            Check();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Action("button4");
+            Check();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Action("button5");
+            Check();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Action("button6");
+            Check();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Action("button7");
+            Check();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Action("button8");
+            Check();
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Action("button9");
+            Check();
+        }
+
+        private void pnlMain_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
